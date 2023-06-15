2abdbd7fbef68ac8cb6fda2fd5711837cedb62a0
JeysonCotrina <133979906+JeysonCotrina@users.noreply.github.com>
2023-06-14 21:00:42 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -4,7 +4,7 @@ class Biblioteca():
     def __init__(self):
         self.conexion= mysql.connector.connect(host='localhost',user='root',password='',database='prestamos')
 
-    """def __str__(self):
+    def __str__(self):
         datos = self.consulta_usuario()
         aux = ''
         for columna in datos:
@@ -17,15 +17,22 @@ class Biblioteca():
         cur.execute(sql)
         datos=cur.fetchall()
         cur.close()
-        return datos"""
+        return datos
 
-    def identificar(self,rut_usuario):
+    def identificar_usuario(self,rut,contrasena):
         cur=self.conexion.cursor()
-        cur.execute(f"""select id_tipo from usuario where rut_usuario ='{rut_usuario}';""")
-        n = cur.fetchone()
-        cur.execute(f"""select nombre from usuario where rut_usuario ='{rut_usuario}';""")
-        o = cur.fetchone()
-        while n != None:
-            j, = n
-            w, = o
-            return j,w
+        cur.execute(f"""select rut_usuario from usuario where rut_usuario ='{rut}' and contrasena_usuario ='{contrasena}';""")
+        datos=cur.fetchall()
+        return datos
+    
+    def identificar_docente(self,rut,contrasena):
+        cur=self.conexion.cursor()
+        cur.execute(f"""select rut_cliente,id_tipo_cliente,contrasena_cliente from cliente where rut_cliente ='{rut}' and id_tipo_cliente={1} and contrasena_cliente ='{contrasena}';""")
+        datos=cur.fetchall()
+        return datos
+    
+    def identificar_alumno(self,rut,contrasena):
+        cur=self.conexion.cursor()
+        cur.execute(f"""select cliente.rut_cliente,cliente.id_tipo_cliente from cliente where rut_cliente ='{rut}' and id_tipo_cliente={2} and contrasena_cliente ='{contrasena}';""")
+        datos=cur.fetchall()
+        return datos
