f17f01067e2847bbaccf895f5f0035621e97faa6
JeysonCotrina <133979906+JeysonCotrina@users.noreply.github.com>
2023-06-29 07:54:32 -0400

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -3,20 +3,29 @@ import mysql.connector
 class Biblioteca():
     def __init__(self):
         self.conexion= mysql.connector.connect(host='localhost',user='root',password='',database='prestamos')
-
     
+    def InfoCliente(self,rut):
+        cur=self.conexion.cursor()
+        cur.execute(f"SELECT nombre,apellido,telefono,correo,id_tipo_cliente,multas FROM cliente WHERE rut = '{rut}'")
+        datos=cur.fetchone()
+        return datos
+    
+class Encargado(Biblioteca):
     def identificar_usuario(self,rut,contrasena):
         cur=self.conexion.cursor()
         cur.execute(f"""select rut from usuario where rut ='{rut}' and contrasena ='{contrasena}';""")
         datos=cur.fetchall()
         return datos
-    
+
+class Docente(Biblioteca):
     def identificar_docente(self,rut,contrasena):
         cur=self.conexion.cursor()
         cur.execute(f"""select rut,id_tipo_cliente,contrasena from cliente where rut ='{rut}' and id_tipo_cliente={1} and contrasena ='{contrasena}';""")
         datos=cur.fetchall()
         return datos
     
+
+class Alumno(Biblioteca):
     def identificar_alumno(self,rut,contrasena):
         cur=self.conexion.cursor()
         cur.execute(f"""select rut,id_tipo_cliente from cliente where rut ='{rut}' and id_tipo_cliente={2} and contrasena ='{contrasena}';""")
